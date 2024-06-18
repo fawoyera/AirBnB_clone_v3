49ddf1591e91ce0e63d5454fb4e7f37b30c2d3dc
fawoyera <femiawoyera@gmail.com>
2024-06-18 12:45:47 -0700

    add route to count no of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,20 @@ from flask import jsonify
 def status():
     """method to return status of api"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """method to return the no of each objects by type"""
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.state import State
+    from models.user import User
+    stat_dict = {"amenities": storage.count(Amenity),
+                 "cities": storage.count(City),
+                 "places": storage.count(Place),
+                 "states": storage.count(State),
+                 "users": storage.count(User)}
+    return jsonify(stat_dict)
