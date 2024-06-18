235bcea1ddb6ea061eeef27cc4e61f40b63817af
fawoyera <femiawoyera@gmail.com>
2024-06-18 13:07:37 -0700

    update route to count objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,11 +20,13 @@ def stats():
     from models.amenity import Amenity
     from models.city import City
     from models.place import Place
+    from models.review import Review
     from models.state import State
     from models.user import User
     stat_dict = {"amenities": storage.count(Amenity),
                  "cities": storage.count(City),
                  "places": storage.count(Place),
+                 "reviews": storage.count(Review),
                  "states": storage.count(State),
                  "users": storage.count(User)}
     return jsonify(stat_dict)
